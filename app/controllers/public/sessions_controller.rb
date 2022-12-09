5661c0050b68f53f40db5a1c9cbf4a20cdea210c
EC2 Default User <114875327+kana6234@users.noreply.github.com>
2022-12-09 19:19:19 +0000

    [add]マイページの会員情報の表示とプロフィール編集機能、退会機能の追加

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+  before_action :user_state, only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -18,10 +19,18 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  def user_state
+    @user = User.find_by(email: params[:user][:email])
+    if @user && @user.valid_password?(params[:user][:password])
+      if @user.is_deleted?
+        redirect_to "/users/sign_up"
+      end
+    end
+  end
 end
--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,16 +1,38 @@
 class Public::UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(current_user.id)
   end
 
   def update
+    @user = User.find(current_user.id)
+    if @user.update(user_params)
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
   end
 
   def unsubscribe
   end
 
   def withdraw
+    @user = User.find(current_user.id)
+    @user.update(deleted_params)
+    reset_session
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :profile_image, :introduction)
+  end
+
+  def deleted_params
+    params.require(:user).permit(:is_deleted)
   end
 end
