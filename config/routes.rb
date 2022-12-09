420fde759375c2f09207946b2a8c37e4524595f3
EC2 Default User <114875327+kana6234@users.noreply.github.com>
2022-12-09 15:06:10 +0000

    [add]homesコントローラとusersコントローラの作成

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,13 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
+  scope module: :public do
+    root 'homes#top'
+    get 'users/information/edit' => 'users#edit'
+    patch 'users/information' => 'users#update'
+    get 'users/unsubscribe'
+    patch 'users/withdraw'
+    resources :users, only:[:show]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
