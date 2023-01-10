13ee74907c1fa7a6f8847ec36dd052603daf8118
EC2 Default User <114875327+kana6234@users.noreply.github.com>
2023-01-10 15:21:41 +0000

    [add]BootstrapとFontAwesomeの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,12 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-require('jquery')
+import "jquery";
 import "cocoon";
+import "popper.js";
+import "bootstrap";
+import "../stylesheets/application"
+import '@fortawesome/fontawesome-free/js/all'
 
 Rails.start()
 Turbolinks.start()
