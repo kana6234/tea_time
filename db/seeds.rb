d6bbbbb7a060eb2073b975570294eecac84d4d3d
EC2 Default User <114875327+kana6234@users.noreply.github.com>
2023-02-11 14:42:33 +0000

    [add]テストデータの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = User.create!(
+  [
+    {email: 'olivia@test.com', name: 'Olivia', introduction: '毎日紅茶を飲むぐらい好きです。', password: 'password', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user1.jpg"), filename:"sample-user1.jpg")},
+    {email: 'james@test.com', name: 'James', introduction: '紅茶専門店で働いています。', password: 'password', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user2.jpg"), filename:"sample-user2.jpg")},
+    {email: 'lucas@test.com', name: 'Lucas', introduction: '最近ハマり始めました。', password: 'password', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user3.jpg"), filename:"sample-user3.jpg")}
+  ]
+)
+
+admins = Admin.create!(
+  [
+    {email: 'admin@test.com', password: 'adminpass'}
+  ]
+)
